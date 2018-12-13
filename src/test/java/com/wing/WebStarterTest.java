ea65964da754ad073c567c86728ae4e2065eba21
walterwing <walterwing@hotmail.com>
2018-12-13 22:11:18 +1300

    Create WebStarterTest.java
    Test class for WebStarter

---
+++ src/test/java/com/wing/WebStarterTest.java
@@ -0,0 +1,22 @@
+package com.wing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Test class for {@link WebStarter}
+ * 
+ * @author Wing
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = WebStarter.class)
+public class WebStarterTest {
+
+	@Test
+	public void application_shouldStartWithoutError() {
+		
+	}
+}
